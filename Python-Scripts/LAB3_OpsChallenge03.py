df33101c06310fe6b190a78c6caf4dea8b2968ad
VascoLucas01 <vascolucas01@gmail.com>
2023-04-13 21:13:55 +0100

    Update LAB3_OpsChallenge03.py

--- Python-Scripts/LAB3_OpsChallenge03.py
+++ Python-Scripts/LAB3_OpsChallenge03.py
@@ -4,6 +4,17 @@
 #Purpose: 
 #Why    : 
 
-# Import libraries
+# import libraries
+import os
+
+# variables
+
+# in order to hide my username and password from public repositories, it was created two environment variables
+username = ""
+password = os.environ.get('PASSWORD')
+
+
+
 
 # main
+print(password)
