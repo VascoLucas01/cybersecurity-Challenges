4c4a41b884fe66a97ace09418aa9fe997a906f8e
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-04-19 13:59:45 +0100

    Update LAB4_PasswordComplexityGP.ps1

--- PowerShell-Scripts/LAB4_PasswordComplexityGP.ps1
+++ PowerShell-Scripts/LAB4_PasswordComplexityGP.ps1
@@ -1,10 +1,18 @@
 
 
-
+# export security policy settings from a local or remote machine to a security configuration file (.cfg)
 secedit /export /cfg C:\secpol\securityconfig.cfg
-#$content = Get-Content -Path "C:\secpol\securityconfig.cfg" | ForEach-Object {$_ -replace "PasswordComplexity = 0", "PasswordComplexity = 1"}
+
+# reads the contents of the "C:\secpol\securityconfig.cfg" file, 
+# replaces the string "PasswordComplexity = 0" with "PasswordComplexity = 1"
+# writes the updated contents back to the same file.
 (gc "C:\secpol\securityconfig.cfg").Replace("PasswordComplexity = 0", "PasswordComplexity = 1") | Out-File "C:\secpol\securityconfig.cfg"
+
+
+# configure the local security policy database with the updated security settings stored in the specified configuration file
 secedit /configure /db "C:\windows\security\local.sdb" /cfg "C:\secpol\securityconfig.cfg" /areas SECURITYPOLICY 
+
+# remove the exported configuration file
 rm -force "C:\secpol\securityconfig.cfg" 
 
 
