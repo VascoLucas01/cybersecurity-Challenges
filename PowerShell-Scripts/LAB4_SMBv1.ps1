103a8b1a4c75c2f075682235aa7b3e4874a17c51
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-04-19 23:13:18 +0100

    Update LAB4_SMBv1.ps1

--- PowerShell-Scripts/LAB4_SMBv1.ps1
+++ PowerShell-Scripts/LAB4_SMBv1.ps1
@@ -4,8 +4,8 @@
 
 # ./LAB4_SMBv1.ps1
 
-$path = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
-$name = "SMB1"
+$path     = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
+$name     = "SMB1"
 $property = Get-ItemProperty -Path $path -Name $name -ErrorAction SilentlyContinue
 
 Set-ItemProperty -Path $path -Name $name -Value 0
