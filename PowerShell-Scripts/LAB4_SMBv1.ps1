93f083f64855253f8067febab2935881c8f59578
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-04-19 17:27:39 +0100

    Create LAB4_SMBv1.ps1

---
+++ PowerShell-Scripts/LAB4_SMBv1.ps1
@@ -0,0 +1,11 @@
+# Script : LAB4_SMBv1.ps1
+# Purpose: Automate the process of disable SMBv1
+# Why    : As a measure of security.
+
+# ./LAB4_SMBv1.ps1
+
+$path = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
+$name = "SMB1"
+$property = Get-ItemProperty -Path $path -Name $name -ErrorAction SilentlyContinue
+
+Set-ItemProperty -Path $path -Name $name -Value 0
