cd6484f509ad2cf407950a4e57fdf2bee8e8b9bb
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-04-20 14:55:26 +0100

    Create OpsChallenge05_FileDecryption.ps1

---
+++ PowerShell-Scripts/OpsChallenge05_FileDecryption.ps1
@@ -0,0 +1,28 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=-$True)]
+    [string]$filePath,
+    [Parameter(Mandatory=-$True)]
+    [string]$directoryPath,
+    [Parameter(Mandatory=-$True)]
+    [string]$password
+)
+
+try{
+
+    # if the file path exists
+    if(Test-Path $filePath) {
+        # Decrypt the file
+        Expand-7Zip -ArchiveFileName $filePath -Password $password -TargetPath $directoryPath -Verbose
+        # delete the file that is not encrypted
+        Remove-Item $filePath
+    } 
+    # if the file path does not exists
+    else{
+        Write-Host "__Incorrect file path__"
+    }
+    
+}
+catch{
+    Write-Output $_.Exception.Message
+}
