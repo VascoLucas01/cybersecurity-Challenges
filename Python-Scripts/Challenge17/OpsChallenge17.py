542f66b5667e5ad0ee8b84fdbc331323635cdeb9
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-05-25 19:04:22 +0100

    Update OpsChallenge17.py

--- Python-Scripts/Challenge17/OpsChallenge17.py
+++ Python-Scripts/Challenge17/OpsChallenge17.py
@@ -6,6 +6,7 @@
 
 import time
 import paramiko
+from cryptography.fernet import Fernet
 
 ### funtions
 # Function name: mode1
@@ -60,7 +61,7 @@ def mode3(hostname, username, password, port):
 # Purpose      : Takes a worlist to brute force a SSH connection
 # Arguments    : hostname, username, wordlist, port
 # Return       : none        
-def mode3_v2(hostname, username, wordlist, port):
+def mode3_v2(hostname, username, wordlist, port, fernet):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
@@ -73,6 +74,8 @@ def mode3_v2(hostname, username, wordlist, port):
             print(f"Successful login with password: {str}")
 
             ssh.close()
+            
+            encrypt_string(str,fernet)
             break
         except paramiko.AuthenticationException:
             print(f"Failed login attempt with password: {str}")
@@ -83,8 +86,48 @@ def mode3_v2(hostname, username, wordlist, port):
         except:
             print("\n ----- Something went wrong -----\n")
 
+# Function name: write_key
+# Purpose      : Generates a key that will be used to encrypt and decrypt and save it to a file. Symmetric cryptography.
+# Arguments    : none
+# Return       : none
+def write_key():
+    key = Fernet.generate_key()
+    with open("key","wb") as key_file:
+        key_file.write(key)
+
+# Function name: load_key
+# Purpose      : Loads the key saved in a file
+# Arguments    : none
+# Return       : The key read from the file
+def load_key():
+    return open("key","rb").read()
+
+# Function name: get_key
+# Purpose      : get the key
+# Arguments    : none
+# Return       : The key returned by load_key() function
+def get_key():
+    try:
+        return load_key()
+    except FileNotFoundError:
+        write_key()
+        return load_key()
+    except:
+        print("\n ----- An unexpected error occured during a load key -----\n")
+                  
+# Function name: encrypt_string
+# Purpose      : Encrypts a string and print to the standard output the ciphertext
+# Arguments    : string, fernet
+# Return       : none
+def encrypt_string(string,fernet):
+    encoded_string   = string.encode()
+    encrypted_string = fernet.encrypt(encoded_string).decode()
+    print(f"\nCiphertext: {encrypted_string}\n")
     
 def main(): 
+    # initialize the Fernet class
+    fernet = Fernet(get_key())
+    
     while(True):
         
         user_input = input("\n\n---> Select one of the modes (1,2,3 or q):\n\nMode 1: Offensive; Dictionary Iterator\nMode 2: Defensive; Password Recognized\nMode 3: Authenticate to an SSH server by its IP address\nMode q: Quit\n\n>")
@@ -101,7 +144,7 @@ def main():
                 # mode3(HOSTNAME, USERNAME, PASSWORD, PORT)
                 
                 wordlist = input("\nEnter the path to the wordlist:\n\n>")
-                mode3_v2(HOSTNAME, USERNAME, wordlist, PORT)
+                mode3_v2(HOSTNAME, USERNAME, wordlist, PORT, fernet)
             case "q":
                 print("\nQuitting...")
                 break
