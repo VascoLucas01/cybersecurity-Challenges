15bcd8a504d9924a1c13098b96ad97da9ebe2dda
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-05-25 18:40:10 +0100

    Update OpsChallenge17.py

--- Python-Scripts/Challenge17/OpsChallenge17.py
+++ Python-Scripts/Challenge17/OpsChallenge17.py
@@ -5,6 +5,7 @@
 # Why    : 
 
 import time
+import paramiko
 
 def mode1(file):
     print("\n")
@@ -16,29 +17,52 @@ def mode1(file):
         time.sleep(1)  
         print(w)
     
-
 def mode2(string,wordlist):
     if(string in wordlist):
         print(f"\nThe string \"{string}\" was found!")
     else:
         print(f"\nThe string \"{string}\" was not found!")
     
-
-
-
-while(True):
-    user_input = input("\n\n---> Select one of the modes (1,2, or q):\n\nMode 1: Offensive; Dictionary Iterator\nMode 2: Defensive; Password Recognized\nMode q: Quit\n\n>")
+def mode3(hostname, username, password, port):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    match(user_input):
-        case "1":
-            wordlist = input("\nEnter the path to the wordlist:\n\n>")
-            mode1(wordlist)
-        case "2":
-            string = input("\nEnter a string:\n\n>")
-            wordlist = input("\nEnter the path to the wordlist:\n\n>")
-            mode2(string,wordlist)
-        case "q":
-            print("\nQuitting...")
-            break
-        case _:
-            print("\n ----- You do not entered a correct option. Try again -----\n")
+    try:
+        ssh.connect(hostname=hostname, port=port, username=username, password=password, timeout=3)
+        print(f"Successful login with password: {password}")
+
+        ssh.close()
+    except paramiko.AuthenticationException:
+        print(f"Failed login attempt with password: {password}")
+    except paramiko.SSHException as e:
+        print(f"SSH Exception: {str(e)}")
+    except paramiko.Exception as e:
+        print(f"Error: {str(e)}")
+    except:
+        print("\n ----- Something went wrong -----\n")
+
+    
+def main(): 
+
+    while(True):
+        
+        user_input = input("\n\n---> Select one of the modes (1,2,3 or q):\n\nMode 1: Offensive; Dictionary Iterator\nMode 2: Defensive; Password Recognized\nMode 3: Authenticate to an SSH server by its IP address\nMode q: Quit\n\n>")
+        
+        match(user_input):
+            case "1":
+                wordlist = input("\nEnter the path to the wordlist:\n\n>")
+                mode1(wordlist)
+            case "2":
+                string = input("\nEnter a string:\n\n>")
+                wordlist = input("\nEnter the path to the wordlist:\n\n>")
+                mode2(string,wordlist)
+            case "3":
+                mode3(HOSTNAME, USERNAME, PASSWORD, PORT)
+            case "q":
+                print("\nQuitting...")
+                break
+            case _:
+                print("\n ----- You do not entered a correct option. Try again -----\n")
+            
+if __name__ == "__main__":
+    main()
