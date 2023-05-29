2c529b5c735790a999009da38988a0975cde9d9d
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-05-29 16:16:56 +0100

    Update OpsChallenge18.py

--- Python-Scripts/Challenge18/OpsChallenge18.py
+++ Python-Scripts/Challenge18/OpsChallenge18.py
@@ -25,6 +25,10 @@ def mode1(file):
         print(w)
     
 
+# Function name: mode2
+# Purpose      : Verifies if the string passed as an argument is in the wordlist passed as another argument
+# Arguments    : string, wordlist
+# Return       : none
 def mode2(string,wordlist):
     if(string in wordlist):
         print(f"\nThe string \"{string}\" was found!")
