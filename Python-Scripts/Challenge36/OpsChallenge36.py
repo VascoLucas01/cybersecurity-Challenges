8dbed0e969feb0c2d9e42260444f13c87984b512
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-07-11 18:18:32 +0100

    Create OpsChallenge36.py

---
+++ Python-Scripts/Challenge36/OpsChallenge36.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Script : OpsChallenge36.py
+# Purpose:
+# Why    :
+
+def main():
+
+    url_ip      = input("Enter the URL or IP address:\n\t> ")
+    port_number = input("\nEnter the port number:\n\t> ")
+
+
+if __name__ == "__main__":
+    main()
