30ba23ec144c9eff53707d32d56550bb252e4bac
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-07-15 10:35:48 -0400

    Update OpsChallenge37.py

--- Python-Scripts/Challenge37/OpsChallenge37.py
+++ Python-Scripts/Challenge37/OpsChallenge37.py
@@ -1,1 +1,32 @@
-####
+#!/usr/bin/python3
+
+# Script : OpsChallenge37.py
+# Purpose:
+# Why    :
+
+import requests
+
+def print_cookies(cookies):
+
+    for cookie in cookies:
+        print('cookie domain = ' + cookie.domain)
+        print('cookie name = ' + cookie.name)
+        print('cookie value = ' + cookie.value)
+        print('******************************************')
+
+def main():
+
+    url = 'https://google.com'
+    
+    response = requests.get(url)
+
+    cookies = response.cookies
+
+    print_cookies(cookies)
+
+    response = requests.get(url, cookies=cookies)
+
+    print_cookies(cookies)
+
+if __name__ == "__main__":
+    main()
